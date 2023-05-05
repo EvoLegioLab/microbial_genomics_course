e8d3cd6d6355a20de4fd7b7cbdb511c23edf678a
Lionel Guy <lionel.guy@imbim.uu.se>
2023-05-05 16:47:38 +0200

    fixing yaml

--- bin/util.R
+++ bin/util.R
@@ -3,3 +3,11 @@ library(sandpaper)
 check_lesson()
 build_lesson(".", rebuild = TRUE, quiet = FALSE)
 serve()
+
+
+rmarkdown::pandoc_version()
+tmp <- tempfile()
+sandpaper::no_package_cache()
+sandpaper::create_lesson(tmp, open = FALSE)
+sandpaper::build_lesson(tmp, preview = FALSE, quiet = TRUE)
+fs::dir_tree(tmp, recurse = 1)
