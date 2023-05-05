03abcbf904cbb7bc027c2f0dd6667ac9f9a019f9
Lionel Guy <lionel.guy@imbim.uu.se>
2023-05-05 22:56:07 +0200

    smaller changes in util.R

--- bin/util.R
+++ bin/util.R
@@ -1,8 +1,7 @@
 ## Testing stuff around
-library(sandpaper)
-check_lesson()
-build_lesson(".", rebuild = TRUE, quiet = FALSE)
-serve()
+sandpaper::check_lesson()
+sandpaper::build_lesson(".", rebuild = TRUE, quiet = FALSE)
+sandpaper::serve()
 
 
 rmarkdown::pandoc_version()
@@ -11,3 +10,12 @@ sandpaper::no_package_cache()
 sandpaper::create_lesson(tmp, open = FALSE)
 sandpaper::build_lesson(tmp, preview = FALSE, quiet = TRUE)
 fs::dir_tree(tmp, recurse = 1)
+
+# register the repositories for The Carpentries and CRAN
+options(repos = c(
+  carpentries = "https://carpentries.r-universe.dev/",
+  CRAN = "https://cran.rstudio.com/"
+))
+
+# Install the template packages to your R library
+install.packages(c("sandpaper", "varnish", "pegboard", "tinkr"))
